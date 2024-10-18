aaac9f35b6483eaed3dcbfc06a00337a335727d0
Habeeb Yunus Habeeb <habeebdh1@outlook.com>
2024-10-18 13:57:41 +0100

    Add Admin Otp Modal

--- app/page.tsx
+++ app/page.tsx
@@ -2,50 +2,47 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 import { PatientForm } from '@/components/forms/PatientForm';
-// import { PasskeyModal } from '@/components/PasskeyModal';
+import PasskeyModal from '@/components/PasskeyModal';
 
-const Home = () =>
-  // { searchParams }: SearchParamProps
+const Home = ({ searchParams }: SearchParamProps) => {
+  const isAdmin = searchParams?.admin === 'true';
 
-  {
-    // const isAdmin = searchParams?.admin === 'true';
+  return (
+    <div className="flex h-screen max-h-screen">
+      {isAdmin && <PasskeyModal />}
 
-    return (
-      <div className="flex h-screen max-h-screen">
-        {/* {isAdmin && <PasskeyModal />} */}
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
+            alt="patient"
+            className="mb-12 h-10 w-fit"
+          />
 
-        <section className="remove-scrollbar container my-auto">
-          <div className="sub-container max-w-[496px]">
-            <Image
-              src="/assets/icons/logo-full.svg"
-              height={1000}
-              width={1000}
-              alt="patient"
-              className="mb-12 h-10 w-fit"
-            />
+          <PatientForm />
 
-            <PatientForm />
-
-            <div className="text-14-regular mt-20 flex justify-between">
-              <p className="justify-items-end text-dark-600 xl:text-left">
-                © 2024 CarePluse
-              </p>
-              <Link href="/?admin=true" className="text-green-500">
-                Admin
-              </Link>
-            </div>
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              © 2024 CarePluse
+            </p>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
           </div>
-        </section>
+        </div>
+      </section>
 
-        <Image
-          src="/assets/images/onboarding-img.png"
-          height={1000}
-          width={1000}
-          alt="patient"
-          className="side-img max-w-[50%]"
-        />
-      </div>
-    );
-  };
+      <Image
+        src="/assets/images/onboarding-img.png"
+        height={1000}
+        width={1000}
+        alt="patient"
+        className="side-img max-w-[50%]"
+      />
+    </div>
+  );
+};
 
 export default Home;
